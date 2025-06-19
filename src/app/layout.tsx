fd41ee0510e98263be1a9265d7c29be49a930c38
SuriyaRamesh <suriya.ram@anywhere.co>
2025-06-19 19:52:03 +0530

    build: add common header and footer in core layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header>Hello! I'm the header!</header>
         {children}
+        <footer>Hey! I'm the footer</footer>
       </body>
     </html>
   );
