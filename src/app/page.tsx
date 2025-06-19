c4d896cf8440f1852efeab7e181ef5e856245965
SuriyaRamesh <suriya.ram@anywhere.co>
2025-06-19 19:46:31 +0530

    build: add homepage main content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 export default function Home() {
   return (
-    <div>
+    <div className="text-center pt-12">
+      <h1 className="text-3xl capitalize font-bold mb-4">Welcome to my blog!</h1>
+      <p className="text-[16px]">This is a Next.js blog.</p>
     </div>
   );
 }
