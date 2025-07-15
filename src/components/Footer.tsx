5e52138b66598fbf00154ae478da6801e8892f5f
SuriyaRamesh <suriya.ram@anywhere.co>
2025-07-15 13:19:08 +0530

    Add header and footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="flex justify-center items-center mt-auto px-5 border-t border-black/10 h-10">
+        Footer
+    </div>
+  );
+}   
