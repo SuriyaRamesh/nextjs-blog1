50e4f3f90c3f1cf7f51482fad88ba57fa4404420
SuriyaRamesh <suriya.ram@anywhere.co>
2025-07-17 13:47:54 +0530

    build: add eg post fetching from prisma db

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,8 +1,13 @@
-export default async function Page() {
+import Link from "next/link";
 
+export default async function Page() {
+    // Fetching data from an external API
     const response = await fetch("https://dummyjson.com/posts");
     const data = await response.json();
     console.log(data);
+
+    // To get data from your own Database (using ORM Prisma here)
+    // const posts = await prisma.post.findmany(); 
 
     return (
         <div className="text-center pt-12">
@@ -10,9 +15,10 @@ export default async function Page() {
             <ul>
                 {data.posts.map((post: {id: number, title: string}) => (
                     <li key={post.id} className="mb-2">
-                        <a href={`/posts/${post.id}`} className="text-blue-500 hover:underline">
+                        {/* <a href={`/posts/${post.id}`} className="text-blue-500 hover:underline">
                             {post.title}
-                        </a>
+                        </a> */}
+                        <Link href={`/posts/${post.id}`}>{post.title}</Link>
                     </li>
                 ))}
             </ul>
